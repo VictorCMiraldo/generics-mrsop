e80dc8239b03c893f6ce01bd96c07744a27afa6c
Alejandro Serrano <trupill@gmail.com>
2018-02-02 09:55:38 +0100

    Separate common parts and implement Show instances

--- Outline.hs
+++ Outline.hs
@@ -1,6 +1,6 @@
-{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# OPTIONS_GHC -cpp #-}
 {-# LANGUAGE RankNTypes #-}
@@ -10,6 +10,8 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE GADTs     #-}
+-- UndecidableInstances needed for Show of Fix
+{-# LANGUAGE UndecidableInstances #-}
 module Outline where
 
 import Data.Proxy
@@ -17,12 +19,14 @@ import Data.Proxy
 import GHC.TypeLits (TypeError , ErrorMessage(..))
 
 data Nat = S Nat | Z
+         deriving (Eq, Show)
 
 -- * Codes
 
 data Atom
   = K Kon
   | I Nat
+  deriving (Eq, Show)
 
 -- can't use type synonyms as they are
 -- not promoted to kinds with -XDataKinds yet.
@@ -34,6 +38,8 @@ data Atom
 -- * Opaque Types
 
 data Kon = KInt
+         deriving (Eq, Show)
+
 type family Konst (k :: Kon) :: * where
   Konst KInt = Int
 
@@ -44,13 +50,28 @@ data NP :: (k -> *) -> [k] -> * where
   NP0  :: NP p '[]
   (:*) :: p x -> NP p xs -> NP p (x : xs)
 
+instance Show (NP p '[]) where
+  show NP0 = "NP0"
+instance (Show (p x), Show (NP p xs)) => Show (NP p (x : xs)) where
+  showsPrec p (v :* vs) = showParen (p > 5) $ showsPrec 5 v . showString " :* " . showsPrec 5 vs
+
 data NS :: (k -> *) -> [k] -> * where
   There :: NS p xs -> NS p (x : xs)
   Here  :: p x     -> NS p (x : xs)
 
+instance Show (NS p '[]) where
+  show _ = error "This code is unreachable"
+deriving instance (Show (p x), Show (NS p xs)) => Show (NS p (x : xs))
+
 data NA  :: (Nat -> *) -> Atom -> * where
   NA_I :: fam k   -> NA fam (I k) 
   NA_K :: Konst k -> NA fam (K k)
+
+-- https://stackoverflow.com/questions/9082642/implementing-the-show-class
+instance (Show (fam k))   => Show (NA fam (I k)) where
+  showsPrec p (NA_I v) = showParen (p > 10) $ showString "NA_I " . showsPrec 11 v
+instance (Show (Konst k)) => Show (NA fam (K k)) where
+  showsPrec p (NA_K v) = showParen (p > 10) $ showString "NA_K " . showsPrec 11 v
 
 type Rep (fam :: Nat -> *) = NS (Poa fam)
 type Poa (fam :: Nat -> *) = NP (NA fam)
@@ -80,6 +101,8 @@ data Constr :: Nat -> [k] -> * where
   CS :: Constr n xs -> Constr (S n) (x : xs)
   CZ ::                Constr Z     (x : xs)
 
+deriving instance Show (Constr n xs)
+
 inj :: Constr n sum -> Poa fam (Lkup n sum) -> Rep fam sum
 inj CZ     poa = Here poa
 inj (CS c) poa = There (inj c poa)
@@ -87,12 +110,6 @@ inj (CS c) poa = There (inj c poa)
 data View :: (Nat -> *) -> Sum -> * where
 --Tag :: (c : Constr sum) -> Poa fam (TypeOf c) -> View fam sum
   Tag :: Constr n sum -> Poa fam (Lkup n sum) -> View fam sum
-{-
-deriving instance Show (NA n s)
-deriving instance Show (Poa n s)
-deriving instance Show (Constr n s)
-deriving instance Show (View n s)
--}
 
 sop :: Rep fam sum -> View fam sum
 sop (Here  poa) = Tag CZ poa
@@ -109,7 +126,14 @@ type family Lkup (n :: Nat) (ks :: [k]) :: k where
 newtype Fix (fam :: Family) (n :: Nat)
   = Fix { unFix :: Rep (Fix fam) (Lkup n fam) }
 
+deriving instance Show (Rep (Fix fam) (Lkup n fam)) => Show (Fix fam n)
+
 -- * Cannonical Example
+
+data R a = a :>: [R a] deriving Show
+
+value :: R Int
+value = 1 :>: [2 :>: [], 3 :>: []]
 
 type List = '[ '[] , '[I (S Z) , I Z] ]
 type RT   = '[ '[K KInt , I Z] ]
@@ -124,34 +148,3 @@ cons x xs = Fix (There $ Here (NA_I x :* NA_I xs :* NP0))
 
 fork :: Int -> Fix Rose Z -> Fix Rose (S Z)
 fork x xs = Fix (Here (NA_K x :* NA_I xs :* NP0))
-
--- * Multirec
-
-class Element ty (fam :: Family) where
-  type Idx ty fam :: Nat
-
-  from :: ty -> Fix fam (Idx ty fam)
-  to   :: Fix fam (Idx ty fam) -> ty
-
--- * Usage
-
-data R a = a :>: [R a] deriving Show
-
-instance Element [R Int] Rose where
-  type Idx [R Int] Rose = Z
-  
-  from []     = nil 
-  from (x:xs) = cons (from x) (from xs)
-
-  to (Fix x) = case sop x of
-                    Tag CZ p -> []
-                    Tag (CS CZ) (NA_I vx :* NA_I vxs :* NP0)
-                       -> (to vx : to vxs)
-
-instance Element (R Int) Rose where
-  type Idx (R Int) Rose = (S Z)
-
-  from (i :>: is) = fork i (from is)
-  to (Fix (Here (NA_K i :* NA_I xs :* NP0))) = i :>: to xs
-
-
