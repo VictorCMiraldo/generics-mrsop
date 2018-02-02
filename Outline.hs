e4e539f5ae73f259e712dfdc5bd874797741506b
Alejandro Serrano <trupill@gmail.com>
2018-02-02 11:06:03 +0100

    Add constant interpretation

--- Outline.hs
+++ Outline.hs
@@ -23,25 +23,17 @@ data Nat = S Nat | Z
 
 -- * Codes
 
-data Atom
-  = K Kon
+data Atom kon
+  = K kon
   | I Nat
   deriving (Eq, Show)
 
 -- can't use type synonyms as they are
 -- not promoted to kinds with -XDataKinds yet.
 -- https://ghc.haskell.org/trac/ghc/wiki/GhcKinds#Kindsynonymsfromtypesynonympromotion
-#define Prod    [Atom]
-#define Sum     [Prod]
-#define Family  [Sum]
-
--- * Opaque Types
-
-data Kon = KInt
-         deriving (Eq, Show)
-
-type family Konst (k :: Kon) :: * where
-  Konst KInt = Int
+#define Prod(kon)   [Atom kon]
+#define Sum(kon)    [Prod(kon)]
+#define Family(kon) [Sum(kon)]
 
 -- * Interpretation of Codes
 
@@ -65,22 +57,22 @@ instance (Show (p x), Show (NS p xs)) => Show (NS p (x : xs)) where
   show (There r) = 'T' : show r
   show (Here  x) = "H (" ++ show x ++ ")"
 
-data NA  :: (Nat -> *) -> Atom -> * where
-  NA_I :: fam k   -> NA fam (I k) 
-  NA_K :: Konst k -> NA fam (K k)
+data NA  :: (kon -> *) -> (Nat -> *) -> Atom kon -> * where
+  NA_I :: fam k -> NA ki fam (I k) 
+  NA_K :: ki  k -> NA ki fam (K k)
 
 -- https://stackoverflow.com/questions/9082642/implementing-the-show-class
-instance (Show (fam k))   => Show (NA fam (I k)) where
+instance (Show (fam k)) => Show (NA ki fam (I k)) where
   showsPrec p (NA_I v) = showParen (p > 10) $ showString "I-" . showsPrec 11 v
-instance (Show (Konst k)) => Show (NA fam (K k)) where
+instance (Show (ki  k)) => Show (NA ki fam (K k)) where
   showsPrec p (NA_K v) = showParen (p > 10) $ showString "K " . showsPrec 11 v
 
-type Rep (fam :: Nat -> *) = NS (Poa fam)
-type Poa (fam :: Nat -> *) = NP (NA fam)
+type Rep (ki :: kon -> *) (fam :: Nat -> *) = NS (Poa ki fam)
+type Poa (ki :: kon -> *) (fam :: Nat -> *) = NP (NA  ki fam)
 
 type f :-> g = forall n . f n -> g n
 
-hmapNA :: f :-> g -> NA f a -> NA g a
+hmapNA :: f :-> g -> NA ki f a -> NA ki g a
 hmapNA nat (NA_I f) = NA_I (nat f)
 hmapNA nat (NA_K i) = NA_K i
 
@@ -92,7 +84,7 @@ hmapNS :: f :-> g -> NS f ks -> NS g ks
 hmapNS f (Here p) = Here (f p)
 hmapNS f (There p) = There (hmapNS f p)
 
-hmapRep :: f :-> g -> Rep f c -> Rep g c
+hmapRep :: f :-> g -> Rep ki f c -> Rep ki g c
 hmapRep f = hmapNS (hmapNP (hmapNA f))
 
 -- * SOP functionality
@@ -105,15 +97,15 @@ data Constr :: Nat -> [k] -> * where
 
 deriving instance Show (Constr n xs)
 
-inj :: Constr n sum -> Poa fam (Lkup n sum) -> Rep fam sum
+inj :: Constr n sum -> Poa ki fam (Lkup n sum) -> Rep ki fam sum
 inj CZ     poa = Here poa
 inj (CS c) poa = There (inj c poa)
 
-data View :: (Nat -> *) -> Sum -> * where
+data View :: (kon -> *) -> (Nat -> *) -> Sum(kon) -> * where
 --Tag :: (c : Constr sum) -> Poa fam (TypeOf c) -> View fam sum
-  Tag :: Constr n sum -> Poa fam (Lkup n sum) -> View fam sum
+  Tag :: Constr n sum -> Poa ki fam (Lkup n sum) -> View ki fam sum
 
-sop :: Rep fam sum -> View fam sum
+sop :: Rep ki fam sum -> View ki fam sum
 sop (Here  poa) = Tag CZ poa
 sop (There s)   = case sop s of
                     Tag c poa -> Tag (CS c) poa
@@ -125,10 +117,10 @@ type family Lkup (n :: Nat) (ks :: [k]) :: k where
   Lkup (S n) (k : ks) = Lkup n ks
   Lkup _     '[]      = TypeError (Text "Lkup index too big")
 
-newtype Fix (fam :: Family) (n :: Nat)
-  = Fix { unFix :: Rep (Fix fam) (Lkup n fam) }
+newtype Fix (ki :: kon -> *) (fam :: Family(kon)) (n :: Nat)
+  = Fix { unFix :: Rep ki (Fix ki fam) (Lkup n fam) }
 
-instance Show (Rep (Fix fam) (Lkup n fam)) => Show (Fix fam n) where
+instance Show (Rep ki (Fix ki fam) (Lkup n fam)) => Show (Fix ki fam n) where
   show (Fix x) = show x
 
 -- * Cannonical Example
@@ -141,13 +133,20 @@ value = 1 :>: [2 :>: [], 3 :>: []]
 type List = '[ '[] , '[I (S Z) , I Z] ]
 type RT   = '[ '[K KInt , I Z] ]
 
+data Kon = KInt
+         deriving (Eq, Show)
+
+data Singl (kon :: Kon) where
+  SInt :: Int -> Singl KInt 
+
 type Rose = '[List , RT]
 
-nil :: Fix Rose Z
+
+nil :: Fix Singl Rose Z
 nil = Fix (Here NP0)
 
-cons :: Fix Rose (S Z) -> Fix Rose Z -> Fix Rose Z
+cons :: Fix Singl Rose (S Z) -> Fix Singl Rose Z -> Fix Singl Rose Z
 cons x xs = Fix (There $ Here (NA_I x :* NA_I xs :* NP0))
 
-fork :: Int -> Fix Rose Z -> Fix Rose (S Z)
-fork x xs = Fix (Here (NA_K x :* NA_I xs :* NP0))
+fork :: Int -> Fix Singl Rose Z -> Fix Singl Rose (S Z)
+fork x xs = Fix (Here (NA_K (SInt x) :* NA_I xs :* NP0))
--- TypeClassOnly.hs
+++ TypeClassOnly.hs
@@ -5,31 +5,31 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE ConstraintKinds #-}
 {-# OPTIONS_GHC -cpp #-}
 module TypeClassOnly where
 
 import Outline
 
-#define Prod    [Atom]
-#define Sum     [Prod]
-#define Family  [Sum]
-
 -- * Multirec
 
-class Element ty (fam :: Family) (ix :: Nat) {- | ty -> fam ix -} where
-  from :: ty -> Fix fam ix
-  to   :: Fix fam ix -> ty
+class Element (ki :: kon -> *) (fam :: [[[Atom kon]]]) (ix :: Nat) ty {- | ty -> fam ix -} where
+  from :: ty -> Fix ki fam ix
+  to   :: Fix ki fam ix -> ty
+
+type HsElement = Element Singl
 
 -- Just to fix the order of variables
 
-from' :: forall fam ix ty. Element ty fam ix => ty -> Fix fam ix
+from' :: forall ki fam ix ty. Element ki fam ix ty => ty -> Fix ki fam ix
 from' = from
-to'   :: forall ty fam ix. Element ty fam ix => Fix fam ix -> ty
+to'   :: forall ty ki fam ix. Element ki fam ix ty => Fix ki fam ix -> ty
 to'   = to
 
 -- * Usage
 
-instance Element [R Int] Rose Z where
+instance Element Singl Rose Z [R Int] where
   from []     = nil 
   from (x:xs) = cons (from x) (from xs)
 
@@ -38,12 +38,12 @@ instance Element [R Int] Rose Z where
                     Tag (CS CZ) (NA_I vx :* NA_I vxs :* NP0)
                        -> (to vx : to vxs)
 
-instance Element (R Int) Rose (S Z) where
+instance Element Singl Rose (S Z) (R Int) where
   from (i :>: is) = fork i (from is)
-  to (Fix (Here (NA_K i :* NA_I xs :* NP0))) = i :>: to xs
+  to (Fix (Here (NA_K (SInt i) :* NA_I xs :* NP0))) = i :>: to xs
 
-value1 :: Fix Rose (S Z)
+value1 :: Fix Singl Rose (S Z)
 value1 = from value
 
 -- Using TypeApplications
-value2 = from' @Rose @(S Z) value
+value2 = from' @Singl @Rose @(S Z) value
