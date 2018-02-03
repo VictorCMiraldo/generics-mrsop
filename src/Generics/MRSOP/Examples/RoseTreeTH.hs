663d2bc8f2e14f599e0b6d637a9161fa0c3e2ae7
Victor Miraldo <victor.miraldo@oracle.com>
2018-02-03 21:22:11 +0100

    I think I figured out how to proceed; still needs implementation though

---
+++ src/Generics/MRSOP/Examples/RoseTreeTH.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE TypeApplications        #-}
+{-# LANGUAGE UndecidableInstances    #-}
+{-# LANGUAGE RankNTypes              #-}
+{-# LANGUAGE FlexibleContexts        #-}
+{-# LANGUAGE FlexibleInstances       #-}
+{-# LANGUAGE FlexibleInstances       #-}
+{-# LANGUAGE GADTs                   #-}
+{-# LANGUAGE TypeOperators           #-}
+{-# LANGUAGE DataKinds               #-}
+{-# LANGUAGE PolyKinds               #-}
+{-# LANGUAGE ScopedTypeVariables     #-}
+{-# LANGUAGE FunctionalDependencies  #-}
+{-# LANGUAGE TemplateHaskell         #-}
+module Generics.MRSOP.Examples.RoseTreeTH where
+
+import Data.Function (on)
+
+import Generics.MRSOP.Base.Internal.NS
+import Generics.MRSOP.Base.Internal.NP
+import Generics.MRSOP.Base.Universe
+import Generics.MRSOP.Base.Class
+import Generics.MRSOP.Konstants
+import Generics.MRSOP.Util
+
+import Generics.MRSOP.TH
+
+
+-- * Haskell first-order RoseTrees
+
+data Rose a = a :>: [Rose a]
+  deriving Show
+
+value1, value2 :: Rose Int
+value1 = 1 :>: [2 :>: [], 3 :>: []]
+value2 = 1 :>: [2 :>: []]
+
+deriveFamily [t| Rose Int |]
--- src/Generics/MRSOP/TH.hs
+++ src/Generics/MRSOP/TH.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE DeriveTraversable #-}
 {-# OPTIONS_GHC -cpp         #-}
 {-# LANGUAGE DeriveFunctor   #-}
 {-# LANGUAGE TemplateHaskell #-}
@@ -10,9 +12,14 @@
 --
 module Generics.MRSOP.TH (deriveFamily) where
 
+import Control.Arrow ((***))
 import Control.Monad
+import Control.Monad.State
+import Control.Monad.Identity
+
 import Language.Haskell.TH
-import Language.Haskell.TH.Syntax hiding (Infix)
+import Language.Haskell.TH.Syntax (liftString)
+
 
 import qualified Data.Map as M
 
@@ -23,9 +30,45 @@ import qualified Data.Map as M
 --    2. the SOP code for each of the datatypes involved
 --    3. One 'Element' instance per datatype
 --    TODO: 4. Metadada information for each of the datatypes involved
-deriveFamily :: Name -> Q [Dec]
-deriveFamily name = do dec <- reifyDec name
-                       main dec
+deriveFamily :: Q Type -> Q [Dec]
+deriveFamily t
+  = do res <- t
+       [d| ty :: String
+           ty = $( liftString (show res) ) |]
+
+-- Sketch;
+--
+--   Given a module:
+--
+--    > module Test where
+--    > data Rose a = Fork a [Rose a]
+--    > $(deriveFamily [t| Rose Int |])
+--
+--  We will see we are looking into deriving a family
+--  for an AppT (ConT Rose) (ConT Int).
+--
+--  Working with a (M.Map STy (Int , DInfo (K + I))) in a state;
+--
+--  0) Translate to a simpler Type-expression, call it STy.
+--  1) Register (AppST (ConST Rose) (ConST Int)) as family index Z
+--  2) reify lhs: [d| data Rose a = Fork a [Rose a] |]
+--      a) reduce rhs of (1): (\a -> Fork a [Rose a]) @ (ConT Int)
+--                        == Fork Int [Rose Int]
+--      b) Take the fields that require processing: [ConT Int , AppST List (AppST Rose Int)]
+--      c) Somehow figure out that (ConT Int) is a Konstant.
+--      d) Look into (AppST List (AppST Rose Int))
+--      e) Is it already processed?
+--      f) If yes, we are done.
+--  3) Register (AppST List (AppST Rose Int))as family index (S Z)
+--  4) reify lhs: [d| data List a = Nil | Cons a (List a) |]
+--      a) reduce rhs of (4): (\a -> Nil | Cons a (List a)) @ (AppST Rose Int)
+--      b) Take the fields of each constructor:
+--           [] , [AppST Rose Int , AppST List (AppST Rose Int)]
+--      c) Notice that both fields of 'Cons' have already
+--         been registered; hence they become: [I Z , I (S Z)]
+--
+
+-- OLD COLD CODE:
 
 ---------------
 -- Internals --
@@ -44,11 +87,12 @@ reifyDec name =
 type DataName  = Name
 type ConName   = Name
 type FieldName = Name
+type Args      = [Name]
 
 -- Datatype info:
 data DTI ty
-  = ADT DataName [ CI ty ]
-  | New DataName (CI ty)
+  = ADT DataName Args [ CI ty ]
+  | New DataName Args (CI ty)
   deriving (Eq , Show , Functor)
 
 -- Constructor info is parametrised by the type of 'Types'.
@@ -60,13 +104,81 @@ data CI ty
   | Record ConName [ (FieldName , ty) ]
   deriving (Eq , Show , Functor)
 
+ciMapM :: (Monad m) => (ty -> m tw) -> CI ty -> m (CI tw)
+ciMapM f (Normal name tys) = Normal name <$> mapM f tys
+ciMapM f (Infix name l r)  = Infix name <$> f l <*> f r
+ciMapM f (Record name tys) = Record name <$> mapM (rstr . (id *** f)) tys
+  where
+    rstr (a , b) = b >>= return . (a,)
+
+dtiMapM :: (Monad m) => (ty -> m tw) -> DTI ty -> m (DTI tw)
+dtiMapM f (ADT name args ci) = ADT name args <$> mapM (ciMapM f) ci
+dtiMapM f (New name args ci) = New name args <$> ciMapM f ci
+
+-- A Simplified version of Language.Haskell.TH
+data STy
+  = AppST STy STy
+  | VarST Name
+  | ConST Name
+  | KonST Name
+  deriving (Eq , Show, Ord)
+
+-- We shall need to keep track of the indexes of the types
+-- we explore.
+
+data Idxs idx
+  = Idxs { idxsNext :: Int
+         , idxsMap  :: M.Map idx (Int , Maybe (DTI STy))
+         }
+
+type IdxsM idx = StateT (Idxs idx)
+
+-- |The actual monad we need to run all of this;
+type M = IdxsM STy Q
+
+-- |Returns the index of a "Name" within the family.
+--  If this name has not been registered yet, returns
+--  a fresh index.
+indexOf :: (Ord idx, Monad m) => idx -> IdxsM idx m Int
+indexOf name
+  = do st <- get
+       case M.lookup name (idxsMap st) of
+         Just i  -> return (fst i)
+         Nothing -> let i = idxsNext st
+                     in put (Idxs (i + 1) (M.insert name (i , Nothing) (idxsMap st)))
+                     >> return i
+
+-- Now, we start processing by extracting a (DTI STy) from
+-- a Haskell Declaration.
+
+convertType :: Type -> M STy
+convertType (AppT a b)  = AppST <$> convertType a <*> convertType b
+convertType (SigT t _)  = convertType t
+convertType (VarT n)    = return (VarST n)
+convertType (ConT n)    = return (ConST n)
+convertType (ParensT t) = convertType t
+convertType _           = fail "convertType: Unsupported Type"
+
+argInfo :: TyVarBndr -> Name
+argInfo (PlainTV  n)   = n
+argInfo (KindedTV n _) = n
+
+-- Extracts a DTI from a Dec
+decInfo :: Dec -> M (DTI Type)
+decInfo (TySynD     name args      ty)     = fail "Type Synonyms not supported"
+decInfo (DataD    _ name args    _ cons _) = ADT name (map argInfo args) <$> mapM conInfo cons
+decInfo (NewtypeD _ name args    _ con _)  = New name (map argInfo args) <$> conInfo con
+decInfo _                                  = fail "Only type declarations are supported"
+
+
 -- Extracts a CI from a Con
-conInfo :: Con -> Q (CI Type)
+conInfo :: Con -> M (CI Type)
 conInfo (NormalC  name ty) = return $ Normal name (map snd ty)
 conInfo (RecC     name ty) = return $ Record name (map (\(s , _ , t) -> (s , t)) ty)
 conInfo (InfixC l name r)
-  = do info <- reifyFixity name
+  = do info <- lift (reifyFixity name)
        case info of
+         -- TODO: incorporate fixity information.
          _ -> return $ Infix name (snd l) (snd r)
 conInfo (ForallC _ _ _) = fail "Existentials not supported"
 #if MIN_VERSION_template_haskell(2,11,0)
@@ -74,19 +186,14 @@ conInfo (GadtC _ _ _)    = fail "GADTs not supported"
 conInfo (RecGadtC _ _ _) = fail "GADTs not supported"
 #endif
 
--- Extracts a DTI from a Dec
-decInfo :: Dec -> Q (DTI Type)
--- decInfo (DataD    _ name (_:_) _ cons _) = fail "Parametrized types not supported" 
--- decInfo (NewtypeD _ name (_:_) _ con _)  = fail "Parametrized types not supported"
--- decInfo (TySynD     name (_:_)   ty)     = fail "Parametrized types not supported"
--- decInfo (TySynD     name []      ty)     = fail "Type Synonyms not supported"
-decInfo (DataD    _ name _    _ cons _) = ADT name <$> mapM conInfo cons
-decInfo (NewtypeD _ name _    _ con _)  = New name <$> conInfo con
-
 testDec :: Q [Dec]
 testDec = [d| data RoseTree = Fork Int (List RoseTree)
               data List a = Nil | Cons a (List a)    
             |]
+
+
+runIdxsM :: (Ord idx, Monad m) => IdxsM idx m a -> m a
+runIdxsM = flip evalStateT (Idxs 0 M.empty)
 
 main :: Dec -> Q [Dec]
 main start
