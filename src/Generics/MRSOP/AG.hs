b52d991f56fac56d9a22f5211e4ed5f1daa21aac
Arian van Putten <aeroboy94@gmail.com>
2018-07-24 17:41:29 +0200

    Clean up stuff

--- src/Generics/MRSOP/AG.hs
+++ src/Generics/MRSOP/AG.hs
@@ -66,28 +66,24 @@ synthesize f = cata alg
       -> AnnFix ki codes phi iy
     alg xs = AnnFix (f (mapRep getAnn xs)) xs
 
+
 -- | Annotate each node with the number of subtrees
 sizeGeneric' ::
      forall ki codes ix. Fix ki codes ix -> AnnFix ki codes (Const (Sum Int)) ix
-sizeGeneric' = synthesize alg
+sizeGeneric' = synthesize sizeAlgebra
   where
-    alg ::
-         forall iy.
-         Rep ki (Const (Sum Int)) (Lkup iy codes)
-      -> Const (Sum Int) iy
-    alg rep =
-      Const 1 `mappend` elimRep (const mempty) (Const . getConst) mconcat rep
+    sizeAlgebra :: Rep ki (Const (Sum Int)) xs -> Const (Sum Int) iy
+    sizeAlgebra = 
+          mappend (Const 1) . elimRep (const mempty) (Const . getConst) mconcat
 
 -- | Count the number of nodes
 sizeGeneric :: forall ki codes ix. Fix ki codes ix -> Const (Sum Int) ix
-sizeGeneric = cata alg
+sizeGeneric = cata sizeAlgebra
   where
-    alg ::
-         forall iy.
-         Rep ki (Const (Sum Int)) (Lkup iy codes)
-      -> Const (Sum Int) iy
-    alg rep =
-      Const 1 `mappend` elimRep (const mempty) (Const . getConst) mconcat rep
+    sizeAlgebra :: Rep ki (Const (Sum Int)) xs -> Const (Sum Int) iy
+    sizeAlgebra = 
+          mappend (Const 1) . elimRep (const mempty) (Const . getConst) mconcat
+
 
 -- | Returns the number of leaves in a tree
 countLeavesGeneric :: forall ki codes ix. Fix ki codes ix -> Const (Sum Int) ix
