cb7730b3b4b927e6854f2cf0949e37e71d7e67bf
Arian van Putten <aeroboy94@gmail.com>
2018-08-09 17:52:39 +0200

    Fix Show instances for Fix

--- src/Generics/MRSOP/Base/Show.hs
+++ src/Generics/MRSOP/Base/Show.hs
@@ -20,7 +20,7 @@ import Generics.MRSOP.Base.Universe
 import Generics.MRSOP.Util
 
 -- https://stackoverflow.com/questions/9082642/implementing-the-show-class
-instance (Show (fam k)) => Show (NA ki fam (I k)) where
+{-instance (Show (fam k)) => Show (NA ki fam (I k)) where
   showsPrec p (NA_I v) = showParen (p > 10) $ showString "I " . showsPrec 11 v
 instance (Show (ki  k)) => Show (NA ki fam (K k)) where
   showsPrec p (NA_K v) = showParen (p > 10) $ showString "K " . showsPrec 11 v
@@ -42,9 +42,37 @@ instance (Show (p x), Show (NS p xs)) => Show (NS p (x : xs)) where
 -- TODO:
 -- This needs undecidable instances. We don't like undecidable instances
 instance Show (NS (PoA ki phi) code) => Show (Rep ki phi code) where
-  show (Rep x) = show x
+  show (Rep x) =
+-}
 
-instance Show (NS (PoA ki (Fix ki codes)) (Lkup ix codes))
-      => Show (Fix ki codes ix)
-    where
-  show (Fix x) = show x
+
+instance (Show1 phi, Show1 ki) => Show (NA ki (AnnFix ki codes phi) a) where
+  show = showNA
+
+showNA :: (Show1 phi, Show1 ki) => NA ki (AnnFix ki codes phi) a -> String
+showNA (NA_I i) = "(NA_I " ++ showFix i ++ ")"
+showNA (NA_K k) = "(NA_K " ++ show1 k ++ ")"
+
+instance (Show1 phi, Show1 ki) => Show (PoA ki (AnnFix ki codes phi) xs) where
+  show = showNP
+
+showNP :: (Show1 phi, Show1 ki) => PoA ki (AnnFix ki codes phi) xs -> String
+showNP NP0 = "NP0"
+showNP (a :* b) = showNA a ++ " :* " ++ showNP b
+
+instance (Show1 phi, Show1 ki) => Show (Rep ki (AnnFix ki codes phi) xs) where
+  show = showRep
+  
+showRep :: (Show1 phi, Show1 ki) => Rep ki (AnnFix ki codes phi) xs -> String
+showRep x =
+  case sop x of
+    Tag c poa -> 
+      "(" ++ show c ++ " " ++ showNP poa ++ ")"
+   
+
+instance (Show1 phi, Show1 ki) => Show (AnnFix ki codes phi ix) where
+  show = showFix
+
+showFix :: (Show1 phi, Show1 ki) => AnnFix ki codes phi ix -> String
+showFix (AnnFix a x) = "(" ++ show1 a ++  " " ++ showRep x  ++ ")"
+
